1775821ca3d3d6c1fe11c7dc43905e79051ce00e
Alestan <48387196+AlestanAlves@users.noreply.github.com>
2021-09-03 20:37:49 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  
+!/bin/bash  
 #Script de backup
 #Autor: Alestan
 #RM: 85046
@@ -13,12 +13,13 @@ total=$(ls ../../srv/backup/*.tar.gz 2>/dev/null| wc -l)
 
 cd ../../srv/samba 
 
-if [ exists == 5 ] ; then
+if [ $total == "5" ] then
   echo "Existe 5 arquivos em backup a ultima data foi excluida"
-elif [ -e "bkp_samba_$dia.tar.gz" ] ; then
+elif [ -e "bkp_samba_$dia.tar.gz" ] then
   echo "O arquivo existe, entao nao foi criado um novo"
 else
   echo "O arquivo não existe e foi guardado na pasta backup"
   tar -zcvf bkp_samba_$dia.tar.gz .
   cp bkp_samba_$DIA.tar.gz ../backup/ 
 fi
+

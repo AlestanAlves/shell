738101aa87cbb7c0a91de8b37c130b1c29ea1c02
Alestan <48387196+AlestanAlves@users.noreply.github.com>
2021-09-02 21:21:17 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -11,7 +11,6 @@ cd ../../srv/samba
 
 if [ -e "bkp_samba_$DIA.tar.gz" ] ; then
   echo "O arquivo existe, entao nao foi criado um novo"
-
 else
   echo "O arquivo não existe e foi guardado na pasta backup"
   tar -zcvf bkp_samba_$DIA.tar.gz .

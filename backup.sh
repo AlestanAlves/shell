2f1e3d5eb4f2eaa774fd9de0c114d0a81b4522e7
Alestan <48387196+AlestanAlves@users.noreply.github.com>
2021-09-03 20:39:52 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -13,7 +13,7 @@ total=$(ls ../../srv/backup/*.tar.gz 2>/dev/null| wc -l)
 
 cd ../../srv/samba 
 
-if [ $total == "5" ] then
+if [ ${total} == "5" ] then
   echo "Existe 5 arquivos em backup a ultima data foi excluida"
 elif [ -e "bkp_samba_$dia.tar.gz" ] then
   echo "O arquivo existe, entao nao foi criado um novo"
@@ -22,4 +22,3 @@ else
   tar -zcvf bkp_samba_$dia.tar.gz .
   cp bkp_samba_$DIA.tar.gz ../backup/ 
 fi
-

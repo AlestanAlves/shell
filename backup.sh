82a31b9c7f7f1940c665d25aeda16364f6e33d65
Alestan <48387196+AlestanAlves@users.noreply.github.com>
2021-09-02 21:17:11 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -9,6 +9,16 @@ DIA=$(date +%d%m%y)
 
 cd ../../srv/samba 
 
-tar -zcvf bkp_samba_$DIA.tar.gz .
+if [ -e "bkp_samba_$DIA.tar.gz" ] ; then
+  echo "o arquivo existe"
+
+else
+  echo "o arquivo não existe"
+  tar -zcvf bkp_samba_$DIA.tar.gz .
+  cp bkp_samba_$DIA.tar.gz ../backup/ 
+fi
  
-cp bkp_samba_$DIA.tar.gz ../backup/ 
+
+#tar -zcvf bkp_samba_$DIA.tar.gz .
+ 
+#cp bkp_samba_$DIA.tar.gz ../backup/ 

164a3a92f80ba78e8296430d37f585b0655e0ccc
Alestan <48387196+AlestanAlves@users.noreply.github.com>
2021-09-03 21:09:06 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -14,7 +14,8 @@ total=$(ls ../../srv/backup/*.tar.gz 2>/dev/null| wc -l)
 cd ../../srv/samba 
 
 if [ ${total} -eq "5" ] ; then
-  echo "Existe 5 arquivos em backup a ultima data foi excluida"
+  echo "Existe 5 arquivos em backup o arquivo mais antigo criado foi excluido"
+  find ../backup* -mtime +1 -exec rm {} \; 
 elif [ -e "bkp_samba_$dia.tar.gz" ] ; then
   echo "O arquivo existe, entao nao foi criado um novo"
 else

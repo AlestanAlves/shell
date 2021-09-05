bb36529092561504e65696b410b582e2890597e0
Alestan <48387196+AlestanAlves@users.noreply.github.com>
2021-09-05 20:41:23 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -14,15 +14,12 @@ total=$(ls ../../srv/backup/*.tar.gz 2>/dev/null| wc -l)
 cd ../../srv/samba 
 
 if [ ${total} -eq "5" ] ; then
-  echo $(date '+%Y %b %d %H:%M')  "---- Existe 5 arquivos em backup o arquivo mais antigo criado foi excluido" >> backup.log
+  echo $(date '+%Y %b %d %H:%M')  "---- Existe 5 arquivos em backup o arquivo mais antigo criado foi excluido" >> backup.log | mail -s "existe 5 arquivos de backup" alestanalves@gmail.com
   find ../backup/ -mtime +5 -exec rm {} \;
-  mail -s $(date '+%Y %b %d %H:%M') "---- Existe 5 arquivos em backup o arquivo mais antigo criado foi excluido"  alestanalves@gmail.com
 elif [ -e "bkp_samba_$dia.tar.gz" ] ; then
-  echo $(date '+%Y %b %d %H:%M')  "----- O arquivo existe, entao nao foi criado um novo" >> backup.log
-  mail -s $(date '+%Y %b %d %H:%M') "----- O arquivo existe, entao nao foi criado um novo" alestanalves@gmail.com
+  echo $(date '+%Y %b %d %H:%M')  "----- O arquivo existe, entao nao foi criado um novo" >> backup.log | mail -s "arquivo existente" alestanalves@gmail.com
 else
-  echo $(date '+%Y %b %d %H:%M') "----- O arquivo de backup gerado com sucesso" >> backup.log
-  mail -s $(date '+%Y %b %d %H:%M') "----- O arquivo de backup gerado com sucesso" alestanalves@gmail.com
+  echo $(date '+%Y %b %d %H:%M') "----- O arquivo de backup gerado com sucesso" >> backup.log | mail -s "O arquivo de backup gerado" alestanalves@gmail.com
   tar -zcvf bkp_samba_$dia.tar.gz .
   cp bkp_samba_$dia.tar.gz ../backup/ 
 fi
